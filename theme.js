951082e06d207cf3fc82da709c10261b1d21eba0
Pierre Hérissé <pierre.herisse@gmail.com>
2019-03-13 03:29:35 +0100

    almost done

---
+++ theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+
+  // example theme.js
+  // add a custom font
+  font: 'Roboto, sans-serif',
+  // custom colors
+  colors: {
+    text: '#0ff',
+    background: 'black',
+    link: '#66ff66',
+    code: '#ff704d'
+  },
+}
